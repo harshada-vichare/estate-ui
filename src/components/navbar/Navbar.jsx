dfaec617a475dd67989c6b95d64b9cb12e824d75
Harshada <justinfo@zoho.com>
2026-04-29 14:53:27 +0530

    code: hamburger menu completed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { useState } from "react";
 import "./navbar.scss";
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav>
       <div className="left">
@@ -19,6 +22,22 @@ export default function Navbar() {
         <a href="/" className="register">
           Sign up
         </a>
+        {/* small screen menu */}
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt="Menu icon"
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
+        </div>
       </div>
     </nav>
   );
