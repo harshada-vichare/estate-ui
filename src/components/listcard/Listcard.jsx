0fef579974958eb879d93decc681ae988f781814
Harshada <justinfo@zoho.com>
2026-04-30 23:51:54 +0530

    code:listing page for projects is created and projects are listed

---
+++ src/components/listcard/Listcard.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./listcard.scss";
+import { Link } from "react-router-dom";
+
+export default function Listcard({ item }) {
+  return (
+    <div className="listcard">
+      <Link to={`/${item.id}`} className="image-container">
+        <img src={item.img} alt="image" />
+      </Link>
+      <div className="text-container">
+        <h2 className="title">
+          <Link to={`/${item.id}`}>{item.title}</Link>
+        </h2>
+        <p className="address">
+          <img src="./pin.png" alt="location" />
+          <span>{item.address}</span>
+        </p>
+        <p className="price">$ {item.price}</p>
+        <div className="bottom">
+          <div className="features">
+            <div className="feature">
+              <img src="/bed.png" alt="bedroom" />
+              <span>{item.bedroom} bedroom</span>
+            </div>
+            <div className="feature">
+              <img src="/bath.png" alt="bathroom" />
+              <span>{item.bathroom} bathroom</span>
+            </div>
+          </div>
+          <div className="icons">
+            <div className="icon">
+              <img src="/save.png" alt="save" />
+            </div>
+            <div className="icon">
+              <img src="/chat.png" alt="chat" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/routes/listpage/ListPage.jsx
+++ src/routes/listpage/ListPage.jsx
@@ -1,6 +1,22 @@
 import React from "react";
+import { listData } from "../../lib/dummydata.js";
+import Filter from "../../components/filter/Filter";
+import Listcard from "../../components/listcard/Listcard";
 import "./listpage.scss";
 
 export default function ListPage() {
-  return <div className="listpage">hello list</div>;
+  const data = listData;
+  return (
+    <div className="listpage">
+      <div className="list-container">
+        <div className="wrapper">
+          <Filter></Filter>
+          {data.map((item) => (
+            <Listcard key={item.id} item={item}></Listcard>
+          ))}
+        </div>
+      </div>
+      <div className="map-container">map</div>
+    </div>
+  );
 }
