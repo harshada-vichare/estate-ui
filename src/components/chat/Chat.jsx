c2b56e830fd29d161196a0befa3a8e463f87ed79
Harshada <justinfo@zoho.com>
2026-05-07 14:16:49 +0530

    code: profile page completed with chat section

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./chat.scss";
 
 export default function chat() {
+  const [chat, setChat] = useState(1);
+
   return (
     <div className="chat">
       <div className="messages">
@@ -47,56 +49,60 @@ export default function chat() {
           <p>Lorem ipsum dolor sit amet...</p>
         </div>
       </div>
-      <div className="chatbox">
-        <div className="top">
-          <div className="user">
-            <img
-              src="https://images.pexels.com/photos/18505244/pexels-photo-18505244.jpeg"
-              alt="user"
-            />
-            <span>John Doe</span>
+      {chat && (
+        <div className="chatbox">
+          <div className="top">
+            <div className="user">
+              <img
+                src="https://images.pexels.com/photos/18505244/pexels-photo-18505244.jpeg"
+                alt="user"
+              />
+              <span>John Doe</span>
+            </div>
+            <div className="close" onClick={() => setChat(null)}>
+              X
+            </div>
           </div>
-          <div className="close">X</div>
+          <div className="center">
+            <div className="chat-message">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chat-message own">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chat-message">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chat-message">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chat-message own">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chat-message">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chat-message own">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chat-message">
+              <p>Lorem ipsum dolor sit amet.</p>
+              <span>1 hour ago</span>
+            </div>
+          </div>
+          <div className="bottom">
+            <textarea></textarea>
+            <button>Send</button>
+          </div>
         </div>
-        <div className="center">
-          <div className="chat-message">
-            <p>Lorem ipsum dolor sit amet.</p>
-            <span>1 hour ago</span>
-          </div>
-          <div className="chat-message own">
-            <p>Lorem ipsum dolor sit amet.</p>
-            <span>1 hour ago</span>
-          </div>
-          <div className="chat-message">
-            <p>Lorem ipsum dolor sit amet.</p>
-            <span>1 hour ago</span>
-          </div>
-          <div className="chat-message">
-            <p>Lorem ipsum dolor sit amet.</p>
-            <span>1 hour ago</span>
-          </div>
-          <div className="chat-message own">
-            <p>Lorem ipsum dolor sit amet.</p>
-            <span>1 hour ago</span>
-          </div>
-          <div className="chat-message">
-            <p>Lorem ipsum dolor sit amet.</p>
-            <span>1 hour ago</span>
-          </div>
-          <div className="chat-message own">
-            <p>Lorem ipsum dolor sit amet.</p>
-            <span>1 hour ago</span>
-          </div>
-          <div className="chat-message">
-            <p>Lorem ipsum dolor sit amet.</p>
-            <span>1 hour ago</span>
-          </div>
-        </div>
-        <div className="bottom">
-          <textarea></textarea>
-          <button>Send</button>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
